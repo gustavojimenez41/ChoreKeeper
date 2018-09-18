f28c77c132474fc1c161e6a549b2376141be24bf
C-Falvo <43222593+C-Falvo@users.noreply.github.com>
2018-09-17 19:53:03 -0500

    Added User list to house and created token when pressed.

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Home.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Home.java
@@ -2,12 +2,28 @@ package com.example.gustavojimenez.chorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
-
+    String users[] = new String [] {"12    User_1","5      User_2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,users);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(Home.this,users[position],Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
