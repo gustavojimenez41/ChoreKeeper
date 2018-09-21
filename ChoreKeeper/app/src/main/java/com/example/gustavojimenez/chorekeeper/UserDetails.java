11ef82fa6848f4c22910ce58e4fe32febb0d9025
C-Falvo <43222593+C-Falvo@users.noreply.github.com>
2018-09-21 18:14:24 -0500

    Pushing on a user goes to another page with a picture

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/UserDetails.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/UserDetails.java
@@ -1,7 +1,12 @@
 package com.example.gustavojimenez.chorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.widget.ImageView;
 
 public class UserDetails extends AppCompatActivity {
 
@@ -9,5 +14,46 @@ public class UserDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+
+        Intent in = getIntent();
+        int index = in.getIntExtra("com.example.gustavojimenez.USER_INDEX", -1);
+
+        if (index > -1);{
+            int pic = getImg(index);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            scaleImg(img,pic);
+
+        }
+    }
+
+    private int getImg(int index){
+        switch (index){
+            case 0: return R.drawable.u1;
+            case 1: return R.drawable.user2;
+            case 2: return R.drawable.user3;
+            default: return -1;
+        }
+    }
+
+    private void scaleImg(ImageView img, int pic){
+
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imgWidth > screenWidth){
+            int ratio = Math.round( (float)imgWidth / (float)screenWidth );
+            options.inSampleSize = ratio;
+        }
+
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+        img.setImageBitmap(scaledImg);
+
     }
 }
