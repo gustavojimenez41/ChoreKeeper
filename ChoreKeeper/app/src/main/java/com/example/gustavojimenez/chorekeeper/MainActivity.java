89c3b574f4c5f2cd5b6916b8344dcb8bd70b9efe
stavo41 <jimenez.gustavo41@gmail.com>
2018-09-22 16:28:29 -0500

    trying to push

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.gustavojimenez.chorekeeper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-    //Button btn = (Button)findViewById(R.id.loginButton);
+    Button registerButton = (Button)findViewById(R.id.loginButton);
+    Button loginButton = (Button)findViewById(R.id.registerButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //if they click register button we will jump to registration activity
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
+
 }
+
--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Register.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Register.java
@@ -2,11 +2,16 @@ package com.example.gustavojimenez.chorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Register extends AppCompatActivity {
-
+    RadioButton joinHouse;
+    RadioGroup selectHouse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        joinHouse = findViewById(R.id.joinHouse);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
