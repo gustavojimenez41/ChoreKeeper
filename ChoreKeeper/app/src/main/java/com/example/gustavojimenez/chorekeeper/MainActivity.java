22ad34291b83a6792ee0a7872a66e9132681f873
stavo41 <jimenez.gustavo41@gmail.com>
2018-09-22 18:15:08 -0500

    added intent

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.gustavojimenez.chorekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+    Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
+    }
 
 }
 
