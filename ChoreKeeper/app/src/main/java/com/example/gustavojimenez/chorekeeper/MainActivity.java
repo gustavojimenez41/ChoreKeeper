44eb7d564a59f8e2bd82fd732937782d51d81ff9
C-Falvo <43222593+C-Falvo@users.noreply.github.com>
2018-09-22 19:17:01 -0500

    Temp set login to go to home activity

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/MainActivity.java
@@ -5,10 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     Button registerButton;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Home.class);
                 startActivity(intent);
                 finish();
             }
