bb104286f9b6994c58fff5903e1472bba976fc7c
C-Falvo <43222593+C-Falvo@users.noreply.github.com>
2018-11-03 15:53:06 -0500

    Assign chores up and running

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/AssignChore.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/AssignChore.java
@@ -3,10 +3,15 @@ package com.example.gustavojimenez.chorekeeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AssignChore extends AppCompatActivity {
+public class AssignChore extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     TextView textElement;
 
@@ -22,12 +27,29 @@ public class AssignChore extends AppCompatActivity {
         newText = "This is where the chore name goes";
         textElement.setText(newText);
 
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.users, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
 
+    }
 
-
-
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
+
+
+
+
+
+
