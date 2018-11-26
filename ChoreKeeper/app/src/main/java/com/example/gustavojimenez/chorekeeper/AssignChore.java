6e1856ed052903e3585c3a3d06342738186d76b9
stavo41 <jimenez.gustavo41@gmail.com>
2018-11-26 07:55:31 -0600

    fixed spinners error

--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/AssignChore.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/AssignChore.java
@@ -66,10 +66,10 @@ public class AssignChore extends AppCompatActivity{
         commentelement.setText(comment);
 
 
-        Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
 
         claim.setOnClickListener(new View.OnClickListener()
         {
--- ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Assign_Reward.java
+++ ChoreKeeper/app/src/main/java/com/example/gustavojimenez/chorekeeper/Assign_Reward.java
@@ -61,10 +61,10 @@ public class Assign_Reward extends AppCompatActivity {
         commentelement.setText(comment);
 
 
-        Spinner spinner = findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);*/
+
 
 
         claim.setOnClickListener(new View.OnClickListener()
